d26181b8ac8c4af0441953052256b1448a7cebec
Thiago Luigi <thiagoluigi7@hotmail.com>
2020-06-05 18:11:16 -0300

    Criação da Classe C

---
+++ ClasseC.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projeto_Thiago_Lima
+{
+    class ClasseC
+    {
+        private String C1;
+        private int C2;
+
+        public void MC1()
+        {
+            Console.WriteLine("MC1");
+        }
+
+        public void MC2()
+        {
+            Console.WriteLine("MC2");
+        }
+    }
+}
