3e29b0f8387cabd40970498102ae0de2c0ea5a84
Thiago Luigi <thiagoluigi7@hotmail.com>
2020-06-05 17:27:56 -0300

    Upload da classe A e da classe B

---
+++ ClasseA.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projeto_Thiago_Lima
+{
+    class ClasseA
+    {
+        private int A1 { get; set; }
+        private float A2 { get; set; }
+
+        public void MA1()
+        {
+            Console.WriteLine("MA1");
+        }
+        public void MA2()
+        {
+            Console.WriteLine("MA2");
+        }
+    }
+}
---
+++ ClasseB.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projeto_Thiago_Lima
+{
+    class ClasseB
+    {
+        private int B1 { get; set; }
+        private float B2 { get; set; }
+
+        public void MB1()
+        {
+            Console.WriteLine("MB1");
+        }
+        public void MB2()
+        {
+            Console.WriteLine("MB2");
+        }
+    }
+}
