02f875fce04a5b38816e220fd04846afbfd7a9bd
Thiago Luigi <thiagoluigi7@hotmail.com>
2020-06-05 18:16:16 -0300

    Commit dos arquivos necessários para test/control

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Projeto_Thiago_Lima
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a1 = new ClasseA();
+            var b1 = new ClasseB();
+            a1.MA1();
+            a1.MA2();
+            b1.MB1();
+            b1.MB2();
+            Console.WriteLine("Sucesso!");
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
