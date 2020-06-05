f714d62489e7547f12f8da795824f70a50958e17
Thiago Luigi <thiagoluigi7@hotmail.com>
2020-06-05 19:06:04 -0300

    Criação da classe D

---
+++ ClasseD.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projeto_Thiago_Lima
+{
+    class ClasseD
+    {
+        private int D1 { get; set; }
+        private int D2 { get; set; }
+        public void MD1() { }
+        public void MD2() { }
+    }
+}
